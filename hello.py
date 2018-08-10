1d8d0a717fb69bb97e304c7ad7ca0e96d1281ea8
csirelia <39493662+csirelia@users.noreply.github.com>
2018-08-10 11:57:03 +0800

    使用request接受header

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+	#return '<h1>Hello World!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
